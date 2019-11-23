7f449c683e9bb732a3ec4788d8d5d7bc06e34bb6
loljoho <loljoho@users.noreply.github.com>
2019-11-23 17:49:59 -0500

    Updates

---
+++ precache-manifest.60a71299bbf78af650dd7bdc6ed732af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3278032b15699b83c552638afc7faa",
+    "url": "/wutangreact/index.html"
+  },
+  {
+    "revision": "c062adeaa444c46a0ddf",
+    "url": "/wutangreact/static/css/main.ef55618d.chunk.css"
+  },
+  {
+    "revision": "ea10ec9e2c062781deb8",
+    "url": "/wutangreact/static/js/2.3f4c1b18.chunk.js"
+  },
+  {
+    "revision": "c062adeaa444c46a0ddf",
+    "url": "/wutangreact/static/js/main.f1cf5a4a.chunk.js"
+  },
+  {
+    "revision": "329070b8d83de2a32392",
+    "url": "/wutangreact/static/js/runtime-main.ac948b74.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0979c0fde3d7cef72072cfa7d71b4bc4.js"
+  "/wutangreact/precache-manifest.60a71299bbf78af650dd7bdc6ed732af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wutangreact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
